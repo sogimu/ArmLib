449a1b028aeb315d3eec21018ca6b98321b90d25
sogimu <sogimu@sogimu-EasyNote-TK85.(none)>
2013-02-26 22:30:09 +0700

    i don't know

--- tests/app.js
+++ tests/app.js
@@ -1,7 +1,21 @@
 window.onload = function() {
     (function(armlib,lib) {
-        r0 = new armlib.class.Rect({
-            name: 'rect0',
+        // action = new armlib.class.Layer({
+            // container: 'container',
+            // name: 'action',
+			// fps: 1,
+			// zindex: 0
+        // });
+		
+		l = new armlib.class.Layer({
+            container: 'container',
+            name: 'l',
+			zindex: 1
+        });
+		
+		
+		r0 = new armlib.class.Rect({
+            name: 'r0',
             width: 5,
             height: 5,
 			x: 400,
@@ -11,44 +25,19 @@ window.onload = function() {
         })
 		.setFunc('onLoad', function() {
 			console.log('onLoad r0');
-		})
+		});
 
 		r1 = new armlib.class.Image({
             name: 'r1',
             src: './img/gras.jpg',
-            //centralPoint: {x: 50, y: 50},
 			width: 500,
             height: 500,
         })
 		.setFunc('onLoad', function() {
 			console.log('onLoad r1');
-			//this._saveChanges();
+			//action.addChild(this);
 		});
-		obj2 = new armlib.class.Object({
-			name: 'obj2',
-            zindex: 1,
-			x: 50,
-			y: 50,
-		})
-		.setFunc('onLoad', function() {
-			console.log('onLoad obj2');			
-		})
-		.addChild(r0);
 		
-		action = new armlib.class.Layer({
-            container: 'container',
-            name: 'action',
-			fps: 1,
-			zindex: 0,
-			synch: false
-        })
-		.setFunc('onLoad', function(){
-			console.log('onLoad action');
-			//this.run();
-		})
-		.addChild(r1)
-		//.addChild(obj2)
-
         r3 = new armlib.class.Image({
             name: 'r3',
             src: './img/nosov_01.jpg',
@@ -59,12 +48,10 @@ window.onload = function() {
             height: 100,
 			//angle: 0.5,
 			centralPoint: {x: 50, y: 50},
-			synch: false
         })
 		.setFunc('onLoad', function() {
 			console.log('onLoad r3');
-			//this._saveChanges();
-		});
+		})
 		
         r5 = new armlib.class.Image({
             name: 'r5',
@@ -73,57 +60,49 @@ window.onload = function() {
             y: 10,
             width: 50,
             height: 50,
-			centralPoint: {x: 125, y: 75},
-			synch: false
+			centralPoint: {x: 125, y: 75}
         })
 		.setFunc('onLoad', function(layer) {
 			console.log('onLoad r5');
-			//layer.run();
 		})
-		.setFunc('onUpdate', function() { this.angle-=0.1;
-		})
-		.setFunc('preDraw', function(ctx) {
-			//ctx.clearRect(-2,-2,this.width+5,this.height+5);
-		})
+		.setFunc('onUpdate', function() { this.angle-=0.1;})
+		.setFunc('preDraw', function(ctx) {})
 		.setFunc('onDraw', function(ctx) {
 			ctx.moveTo(this.x + 25, this.y + 25);
 			ctx.lineTo(this.x + 100,this.y + 100);
 		})
-		r6 = new armlib.class.Image({
-            name: 'r6',
-            src: './img/googlebot-kun-1b.png',
-            x: 150,
-            y: 100,
-            width: 150,
-            height: 150,
-			synch: false
-        })
+
+		obj2 = new armlib.class.Object({
+			name: 'obj2',
+            zindex: 1,
+			x: 50,
+			y: 50,
+		})
 		.setFunc('onLoad', function() {
-			console.log('onLoad r6');
-			//this._saveChanges();
+			console.log('onLoad obj2');
+			//l.addChild(obj2);
 		})
-
-		
-        l = new armlib.class.Layer({
-            container: 'container',
-            name: 'ground',
-			zindex: 1,
-			synch: false
-        })
-		.setFunc('onLoad', function(){
-			console.log('onLoad l');
-			//this.run();
-		})
-		.addChild(r3)
 		.addChild(r5)
-		//.addChild(r6);
+		.addChild(r0)
+		.addChild(r1)
+		.addChild(r3)
+		.Load();
+		//debugger
+		
+		// r6 = new armlib.class.Image({
+            // name: 'r6',
+            // src: './img/googlebot-kun-1b.png',
+            // x: 150,
+            // y: 100,
+            // width: 150,
+            // height: 150,
+			// synch: false
+        // })
+		// .setFunc('onLoad', function() {
+			// console.log('onLoad r6');
+			// l.addChild(this);
+		// })
 
-		armlib.setFunc('onLoad', function() {
-			this.run();
-			console.log('onLoad armlib');
-		});
-		armlib.Load();
-	
     })(armlib,gizmo);
 
 }
